2d5d0d71b879891845ccf85190285117949d5f39
TheLisorML <92590762+TheLisorML@users.noreply.github.com>
2022-10-04 23:11:48 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+/*!
+ * Quara Theme by OllieJW (https://olliejw.me)
+ * License - https://olliejw.me/tos
+ */
+
+var server_name = "Meadow";
+var server_ip = "57.128.136.143";
+var server_port = 30083;
+var ip_copied = "IP copied!";
+
+var server_ip_be = "57.128.136.143";
+var server_port_be = 30083;
+var ip_copied_be = "IP copied!";
+
+var firefly_count = 20;
+
+staff("TheLisorML", "8af4cae6-fe71-35b5-9f03-503d6e5ef892", "Владелец, разработчик");
