aa1bec844b69bae9ebff6344bf06baf7fd53c7b3
TheLisorML <92590762+TheLisorML@users.noreply.github.com>
2022-11-25 20:34:15 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,13 +5,14 @@
 
 var server_name = "Meadow";
 var server_ip = "57.128.136.143";
-var server_port = 30083;
+var server_port = 31043;
 var ip_copied = "IP copied!";
 
-var server_ip_be = "57.128.136.143";
-var server_port_be = 30083;
+var server_ip_be = "*";
+var server_port_be = *;
 var ip_copied_be = "IP copied!";
 
 var firefly_count = 20;
 
 staff("TheLisorML", "8af4cae6-fe71-35b5-9f03-503d6e5ef892", "Владелец, разработчик");
+staff("artynorth", "8d4c9d2a-4e80-3a3e-a3bc-66fbc07fa4ce", "Владелец");
